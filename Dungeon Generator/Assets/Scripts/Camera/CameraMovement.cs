7b70f6c9c43a2a5413c8442590884189602f3d7f
Gabriel Senekovic <Gabriel.Senekovic@gmail.com>
2020-06-14 12:59:17 +0200

    Interior and Exterior house walls are modular

--- Dungeon Generator/Assets/Scripts/Camera/CameraMovement.cs
+++ Dungeon Generator/Assets/Scripts/Camera/CameraMovement.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    public enum CameraMode
+    {
+        TopDown = 0,
+        Side = 1
+    }
     int rotationSideways = 0;
+    [SerializeField]Party party;
+    public CameraMode mode = CameraMode.Side;
 
     void Awake()
     {
         transform.eulerAngles = new Vector3(-45, transform.rotation.y, transform.rotation.z);
-        transform.position = new Vector3(transform.position.x, -1.3f, -8.2f);
+        transform.position = new Vector3(transform.position.x, -8.5f, -8.2f);
     }
     void LateUpdate()
     {
         if(Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.RotateAround(new Vector3(9.5f, 9.5f, 0), Vector3.forward, 1);
+            transform.RotateAround(new Vector3(party.GetPartyLeader().transform.position.x, party.GetPartyLeader().transform.position.y, 0), Vector3.forward, 1);
             rotationSideways++;
         }
         else if(Input.GetKey(KeyCode.RightArrow))
         {
-            transform.RotateAround(new Vector3(9.5f, 9.5f, 0), Vector3.forward, -1);
+            transform.RotateAround(new Vector3(party.GetPartyLeader().transform.position.x, party.GetPartyLeader().transform.position.y, 0), Vector3.forward, -1);
             rotationSideways--;
         }
         if(Input.GetKeyDown(KeyCode.M))
@@ -28,13 +35,15 @@ public class CameraMovement : MonoBehaviour
             if(transform.position.z == -19)
             {
                 transform.eulerAngles = new Vector3(-45, transform.rotation.y, transform.rotation.z);
-                transform.position = new Vector3(transform.position.x, -1.3f, -8.2f);
-                transform.RotateAround(new Vector3(9.5f, 9.5f, 0), Vector3.forward, rotationSideways);
+                transform.position = new Vector3(transform.position.x, -8.5f, -8.2f);
+                transform.RotateAround(new Vector3(party.GetPartyLeader().transform.position.x, party.GetPartyLeader().transform.position.y, 0), Vector3.forward, rotationSideways);
+                mode = CameraMode.Side;
             }
             else
             {
-                transform.position = new Vector3(9.5f, 9.5f, -19);
+                transform.position = new Vector3(party.GetPartyLeader().transform.position.x, party.GetPartyLeader().transform.position.y, -19);
                 transform.eulerAngles = new Vector3(0, 0, rotationSideways);
+                mode = CameraMode.TopDown;
             }
         }
     }
---
+++ Dungeon Generator/Assets/Scripts/UI/Interface/CharacterCreator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCreator : MonoBehaviour
+{
+    // Update is called once per frame
+
+    [SerializeField] Body player;
+
+    [SerializeField] List<Sprite> headSprites; int headSpriteIndex = 0;
+
+    [SerializeField] List<Sprite> eyeSprites; int eyeSpriteIndex = 0;
+
+    [SerializeField] List<Sprite> torsoSprites; int torsoSpriteIndex = 0;
+
+    [SerializeField] List<Sprite> legSprites; int legSpriteIndex = 0;
+
+    private void Start()
+    {
+        player.head.sprite = headSprites[headSpriteIndex];
+
+        player.eyes.eyes[0].sprite = eyeSprites[eyeSpriteIndex];
+        player.eyes.eyes[1].sprite = eyeSprites[eyeSpriteIndex];
+
+        player.torso.sprite = torsoSprites[torsoSpriteIndex];
+        player.legs.sprite = legSprites[legSpriteIndex];
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            headSpriteIndex++; headSpriteIndex %= headSprites.Count;
+            player.head.sprite = headSprites[headSpriteIndex];
+        }
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            eyeSpriteIndex++; eyeSpriteIndex %= eyeSprites.Count;
+            player.eyes.eyes[0].sprite = eyeSprites[eyeSpriteIndex];
+            player.eyes.eyes[1].sprite = eyeSprites[eyeSpriteIndex];
+        }
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            player.eyes.eyes[0].transform.position = new Vector2(player.eyes.eyes[0].transform.position.x - 0.0625f, player.eyes.eyes[0].transform.position.y);
+            player.eyes.eyes[1].transform.position = new Vector2(player.eyes.eyes[1].transform.position.x + 0.0625f, player.eyes.eyes[1].transform.position.y);
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            player.eyes.eyes[0].transform.position = new Vector2(player.eyes.eyes[0].transform.position.x + 0.0625f, player.eyes.eyes[0].transform.position.y);
+            player.eyes.eyes[1].transform.position = new Vector2(player.eyes.eyes[1].transform.position.x - 0.0625f, player.eyes.eyes[1].transform.position.y);
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            player.eyes.eyes[0].transform.position = new Vector2(player.eyes.eyes[0].transform.position.x, player.eyes.eyes[0].transform.position.y + 0.0625f);
+            player.eyes.eyes[1].transform.position = new Vector2(player.eyes.eyes[1].transform.position.x, player.eyes.eyes[1].transform.position.y + 0.0625f);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            player.eyes.eyes[0].transform.position = new Vector2(player.eyes.eyes[0].transform.position.x, player.eyes.eyes[0].transform.position.y - 0.0625f);
+            player.eyes.eyes[1].transform.position = new Vector2(player.eyes.eyes[1].transform.position.x, player.eyes.eyes[1].transform.position.y - 0.0625f);
+        }
+    }
+}
