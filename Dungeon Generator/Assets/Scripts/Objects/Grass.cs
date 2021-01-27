d0c4917d9172e1ed4ed6cbd6c8ade7cfa2243738
Gabriel Senekovic <Gabriel.Senekovic@gmail.com>
2021-01-27 15:43:12 +0100

    Grass started on

---
+++ Dungeon Generator/Assets/Scripts/Objects/Grass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject grass;
+    public Vector2 area;
+    public int grassPerTile;
+    void Start()
+    {
+        for(int i = 0; i < area.x * area.y; i++)
+        {
+            int x = (int)(i % area.x);
+            int y = (int)(i / area.y);
+            //for each tile
+            for(int j = 0; j < grassPerTile; j++)
+            {
+                Instantiate(grass, new Vector2(Random.Range(x, x + 1.0f), Random.Range(y, y+1.0f)), Quaternion.identity, gameObject.transform);
+            }
+        }
+    }
+
+}
